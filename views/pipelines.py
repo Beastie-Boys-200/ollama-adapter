d1c3b71b51b3e9430b1afbe6e07eb2c21b581e49
sidjik <arsenii.milenchuk@student.tuke.sk>
2025-11-29 19:33:16 +0100

    initial version of pipeline for rag text logic

--- views/pipelines.py
+++ views/pipelines.py
@@ -1,3 +1,90 @@
+from . import ollama as ollama_views
+from models.Answer import *
+from models.ollama import OllamaOptions
+from pydantic import BaseModel 
+from controllers import pdf_reader 
+import requests
+import numpy as np
+
+
+FAISS_URL = "http://localhost:8004"
+
+
+def docs_pipeline(query: str, collection_name: str, docs_path: list[Path] | None = None):
+
+
+    # read docs if provided
+    if docs_path is not None: 
+        chunks: list[str] = []
+
+        for doc in docs_path:
+            chunks += pdf_reader.read_pdf(doc)
+
+
+        # make vectors from text
+        embedding = ollama_views.get_embendings(chunks, model='embeddinggemma')
+
+        # update vector db if docs provided
+        if collection_name not in requests.get(f"{FAISS_URL}/faiss/collections/").json():
+            requests.post(
+                f"{FAISS_URL}/faiss/collection/{collection_name}",
+                json = {
+                    "vectors": embedding.tolist(),
+                    "metadata": {
+                        "text": chunks
+                    }
+                }
+            )
+        else:
+            requests.put(
+                f"{FAISS_URL}/faiss/collection/{collection_name}",
+                json = {
+                    "vectors": embedding.tolist(),
+                    "metadata": {
+                        "text": chunks
+                    }
+                }
+            )
+            
+
+    # get embendings from query
+    query_emb = ollama_views.get_embendings([query], model='embeddinggemma')[-1]
+
+    # search top k simple query
+    similar = requests.post(
+        f"{FAISS_URL}/faiss/collections/{collection_name}/similar",
+        json=query_emb.tolist()
+    ).json()[-1]
+
+
+    # generate answer on it
+    return ollama_views.stream_rag_answer(
+        query=RagAnswer(
+            query=query,
+            context=[vocab['text'] for vocab in similar]
+        ),
+        model='llama3:latest'
+    )
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    for token in docs_pipeline(
+        query="What you know about docker",
+        collection_name="devops default",
+        docs_path=[Path('./pdf_samples/devops1.pdf')]
+    ):
+        print(token, end=" ", flush=True)
+
+
 
 
 
